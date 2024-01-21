b247f24de9e596e0bacea18f3cb5a34364669f3d
Renancatalano <kratoscatalano@gmail.com>
2024-01-21 20:04:00 -0300

    projeto dio

---
+++ index.js
@@ -0,0 +1,23 @@
+// Defina o nome e a quantidade de experiência (XP) do herói
+// Defina o nome e a quantidade de experiência (XP) do herói
+let nomeDoHeroi = "Herói Arthur";
+let xpDoHeroi = 1000;
+
+// Utilize uma estrutura de decisão para determinar a mensagem com base na quantidade de XP
+if (xpDoHeroi < 1000) {
+    console.log(nomeDoHeroi + " é do tipo Ferro");
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    console.log(nomeDoHeroi + " é do tipo Bronze");
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    console.log(nomeDoHeroi + " é do tipo Prata");
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) 
+    console.log(nomeDoHeroi + " é do tipo Ouro")
+    else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000)
+    {console.log(nomeDoHeroi + "é do tipo platina");}
+else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000)
+[console.log(nomeDoHeroi + "é do tipo platina")]
+else if (xpDoHeroi >= 9001 && xpdoheroi <= 1000)
+[console.log(nomeDoHeroi + " é do tipo ascendente")]
+else if (xpDoHeroi >= 1001 && xpDoHeroi <= 1100)
+[console.log(nomeDoHeroi + "é do tipo imortal")]
+else {console.log(nomeDoHeroi + "é do tipo radiante")}
